24537ac39c7b94f469a3a47e15889743256c08d7
alexliinc <alexli@indiana.edu>
2017-08-03 12:29:10 -0600

    mappy is up with list

--- projectCode/public/js/app.js
+++ projectCode/public/js/app.js
@@ -1,4 +1,17 @@
-// Document has to be ready 
+// Document has to be ready
 $(document).ready(function() {
   console.log('app.js loaded!');
+  // Moving list items
+
+  function moveItems(origin, dest) {
+    $(origin).find(':selected').appendTo(dest);
+  }
+
+  $('#left').click(function() {
+    moveItems('#sbTwo', '#sbOne');
+  });
+
+  $('#right').on('click', function() {
+    moveItems('#sbOne', '#sbTwo');
+  });
 });
